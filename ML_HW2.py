ac6007c2f42b55d629d37b9c0c8085ee8e7b469e
Maya Gonzalez <mayagonz56@gmail.com>
2022-03-18 16:09:01 -0700

    fixing Ridge Regression

--- ML_HW2.py
+++ ML_HW2.py
@@ -6,11 +6,13 @@
 # Libraries
 import csv
 import math
+from telnetlib import X3PAD
 from tkinter import Y
 import pandas as pd
 import numpy as np
 from scipy import linalg as LA
 import matplotlib.pyplot as plt
+import random
 
 #------------------------------------------------------------------------------
 
@@ -33,6 +35,7 @@ def A_mat(x, deg):
         newCol = np.power(x, (deg-1) -i)
         A = np.c_[A, newCol]
     A = np.c_[A, ones]
+    print(A)
     return A
 
 def LLS_Solve(x,y, deg):
@@ -51,12 +54,11 @@ def LLS_ridge(x,y,deg,lam):
        deg: degree of the polynomial fit.
        lam: parameter for the ridge regression."""
     A = A_mat(x, deg)
-    ATA = A.T@A
-    I = np.identity(len(ATA))
-    lambdaI = np.dot(lam, I)
-    invAdd = LA.inv(np.add(ATA, lambdaI))
-    w = invAdd@(A.T@y)
-    return(w)
+    I = np.identity((A).shape[1])
+    tobe = A.T@A + (lam*I)
+    w = (LA.inv(tobe))@(A.T@y)
+    # w = (LA.inv(A.T@A + (lam*I)))@(A.T@y)
+    return w
 
 def poly_func(data, coeffs):
     """Produce the vector of output data for a polynomial.
@@ -83,30 +85,54 @@ def RMSE(x,y,w):
        x: vector of input data.
        y: vector of output data.
        w: vector of weights."""
-    wtd_rmse = np.sqrt(((y-x)**2).mean())
+    deg = len(w) -1
+    A = A_mat(x, deg)
+    Aw = poly_func(A, w)
+    wtd_rmse = np.sqrt(((y-Aw)**2).mean())
     return wtd_rmse
 
-# 1b. Solve the least squares linear regression problem for the Athens 
-#     temperature data.  Make sure to annotate the plot with the RMSE.
-data = np.genfromtxt('athens_ww2_weather.csv', delimiter=',')
-deg = 1
-x_vals= data[1:, 8:9] # 8th col is min vals
-y_vals = data[1:, 7:8] # 7th col is max vals
-A = A_mat(x_vals, deg)
+# # 1b. Solve the least squares linear regression problem for the Athens 
+# #     temperature data.  Make sure to annotate the plot with the RMSE.
+# data = np.genfromtxt('athens_ww2_weather.csv', delimiter=',')
+# deg = 1
+# x_vals= data[1:, 8:9] # 8th col is min vals
+# y_vals = data[1:, 7:8] # 7th col is max vals
+# A = A_mat(x_vals, deg)
 
-weights = LLS_Solve(x_vals, y_vals, deg) 
-y_pred = poly_func(A, weights)
-rmse = RMSE(x_vals,y_vals,weights)
+# weights = LLS_Solve(x_vals, y_vals, deg) 
+# y_pred = poly_func(A, weights)
+# rmse = RMSE(x_vals,y_vals,weights)
+# plt.scatter( x_vals, y_vals, color = 'b', marker = (5, 1))
+# plt.plot(x_vals, y_pred, color = "r") 
+# plt.title('Temperature Variations in Athens, Greece (Nov. 1944 - Dec. 1945)')
+# plt.text(-2,43,'RMSE: {RMSE_val}'.format(RMSE_val = round(rmse,2)), size = 10, color = 'purple')
+# plt.xlabel("Minimum Temperature C°")
+# plt.ylabel("Maximum Temperature C°")
+# # plt.show()
+# plt.clf()
 
-plt.scatter( x_vals, y_vals, color = 'b', marker = (5, 1))
-plt.plot(x_vals, y_pred, color = "r") 
-plt.title('Temperature Variations in Athens, Greece (Nov. 1944 - Dec. 1945)')
-plt.text(-2,43,'RMSE: {RMSE_val}'.format(RMSE_val = round(rmse,2)), size = 10, color = 'purple')
-# plt.show()
 
 
 # Problem 2 -- Polynomial Regression with the Yosemite Visitor Data
 #------------------------------------------------------------------------------
+
+yosemite_data = pd.read_csv('Yosemite_Visits.csv', thousands=',', index_col =0)
+# print(yosemite_data)
+# gives all value pairs for each month
+yearIndex = [0,10,20,30]
+
+# for each year in Years, get all the month values
+
+# for all 4 years, add each month's data (month can be indexed by i (0-12))
+allYears = []
+for i in yearIndex:
+    allYears.append( yosemite_data.iloc[i][:])
+allYears = np.array(allYears)
+
+year2018 = yosemite_data.loc[2018, :]
+year2008 = yosemite_data.loc[2008, :]
+year1998 = yosemite_data.loc[1998, :]
+year1988 = yosemite_data.loc[1988, :]
 
 # 2a. Create degree-n polynomial fits for 5 years of the Yosemite data, with
 #     n ranging from 1 to 20.  A
@@ -114,133 +140,92 @@ plt.text(-2,43,'RMSE: {RMSE_val}'.format(RMSE_val = round(rmse,2)), size = 10, c
 #     Additionally, create plots comparing the 
 #     training error and RMSE for 3 years of data selected at random (distinct
 #     from the years used for training).
+
+
+deg = 10
+monthRow= np.array([elem for elem in [1,2,3,4,5,6,7,8,9,10,11,12]])
+x_vals = np.c_[monthRow]
+print(x_vals)
+
+A = A_mat(x_vals, deg)
+col2018 = np.c_[year2018]
+y_vals = np.c_[col2018, year2008, year1998, year1988]
+weights = LLS_Solve(x_vals, allYears.T, deg ) 
+y_pred = poly_func(A, weights)
+func = LLS_func(x_vals, y_pred, weights, deg)
+rmse = RMSE(x_vals,y_vals,weights)
+
+def plotYears():
+    plt.scatter(x_vals, year2018, color = 'cyan', marker = '*', s = 40, facecolors = 'none')
+    plt.plot(x_vals, year2018, color = "cyan", label = '2018') 
+    plt.scatter(x_vals, year2008, color = 'purple', marker = '*', s = 40, facecolors = 'none')
+    plt.plot(x_vals, year2008, color = "purple", label = '2008') 
+    plt.scatter(x_vals, year1998, color = '#88c999', marker = '*', s = 40, facecolors = 'none')
+    plt.plot(x_vals, year1998, color = "#88c999", label = '1998') 
+    plt.scatter(x_vals, year1988, color = 'hotpink', marker = '*', s = 40, facecolors = 'none') 
+    plt.plot(x_vals, year1988, color = "hotpink", label = '1988') 
+
+# for loop to create 20 plots for deg 1-20
+# for deg in range(1,21):
+#     plotYears()
+#     A = A_mat(x_vals, deg)
+#     weights = LLS_Solve(x_vals, allYears.T, deg ) 
+#     y_pred = poly_func(A, weights)
+#     plt.plot(x_vals, np.mean(y_pred, axis = 1), color = "r", label = 'Degree-%d Fit' % deg)
+#     handles, labels = plt.gca().get_legend_handles_labels()
+#     plt.legend(handles, labels, loc = 'upper left')
+#     plt.title('Yosemite Visitors with Degree-%d Least Squares (Var. Years)' % deg)
+#     # plt.show()
+
+# used_years = []
+# for i  in range(1,4):
+#     # ensure year is not training year 
+#     year = random.randint(1979,2018)
+#     while (year in [2018,2008,1998,1988] or year in used_years):
+#         year = random.randint(1979,2018)
     
-
-yosemite_data = pd.read_csv('Yosemite_Visits.csv', index_col = 0)
-# gives all value pairs for each month
-Jan = yosemite_data['JAN'].str.split(',')
-Feb = yosemite_data['FEB'].str.split(',')
-Mar = yosemite_data['MAR'].str.split(',')
-Apr = yosemite_data['APR'].str.split(',')
-May = yosemite_data['MAY'].str.split(',')
-Jun = yosemite_data['JUN'].str.split(',')
-Jul = yosemite_data['JUL'].str.split(',')
-Aug = yosemite_data['AUG'].str.split(',')
-Sep = yosemite_data['SEP'].str.split(',')
-Oct = yosemite_data['OCT'].str.split(',')
-Nov = yosemite_data['NOV'].str.split(',')
-Dec = yosemite_data['DEC'].str.split(',')
-
-allMonths = []
-allMonths.append(Jan)
-allMonths.append(Feb)
-years = [2018,2008,1998,1988]
-yearIndex = [0,10,20,30]
-print(Jan.iloc[10][0]) # prints x val for Jan of 2008
-
-# now we need 12 vals for each of the 5 years
-allYears = []
-year2008 = []
-year1998 = []
-year1988 = []
-# for each year in Years, get all the month valies
-for i in (yearIndex): # index through yearIndex
-    print(i)
-    allYears.append(Jan.iloc[i][0])
-    allYears.append(Feb.iloc[i][0])
-    allYears.append(Mar.iloc[i][0])
-    allYears.append(Apr.iloc[i][0])
-    allYears.append(May.iloc[i][0])
-    allYears.append(Jun.iloc[i][0])
-    allYears.append(Jul.iloc[i][0])
-    allYears.append(Aug.iloc[i][0])
-    allYears.append(Sep.iloc[i][0])
-    allYears.append(Oct.iloc[i][0])
-    allYears.append(Nov.iloc[i][0])
-    allYears.append(Dec.iloc[i][0])
-    # # continue for rest of 12 months
-    # year2008.append(Jan.iloc[i][0])
-    # year2008.append(Feb.iloc[i][0])
-    # year2008.append(Mar.iloc[i][0])
-    # year2008.append(Apr.iloc[i][0])
-    # year2008.append(May.iloc[i][0])
-    # year2008.append(Jun.iloc[i][0])
-    # year2008.append(Jul.iloc[i][0])
-    # year2008.append(Aug.iloc[i][0])
-    # year2008.append(Sep.iloc[i][0])
-    # year2008.append(Oct.iloc[i][0])
-    # year2008.append(Nov.iloc[i][0])
-    # year2008.append(Dec.iloc[i][0])
-
-    # year1998.append(Jan.iloc[i][0])
-    # year1998.append(Feb.iloc[i][0])
-    # year1998.append(Mar.iloc[i][0])
-    # year1998.append(Apr.iloc[i][0])
-    # year1998.append(May.iloc[i][0])
-    # year1998.append(Jun.iloc[i][0])
-    # year1998.append(Jul.iloc[i][0])
-    # year1998.append(Aug.iloc[i][0])
-    # year1998.append(Sep.iloc[i][0])
-    # year1998.append(Oct.iloc[i][0])
-    # year1998.append(Nov.iloc[i][0])
-    # year1998.append(Dec.iloc[i][0])
-
-    # year1988.append(Jan.iloc[i][0])
-    # year1988.append(Feb.iloc[i][0])
-    # year1988.append(Mar.iloc[i][0])
-    # year1988.append(Apr.iloc[i][0])
-    # year1988.append(May.iloc[i][0])
-    # year1988.append(Jun.iloc[i][0])
-    # year1988.append(Jul.iloc[i][0])
-    # year1998.append(Aug.iloc[i][0])
-    # year1988.append(Sep.iloc[i][0])
-    # year1988.append(Oct.iloc[i][0])
-    # year1988.append(Nov.iloc[i][0])
-    # year1988.append(Dec.iloc[i][0])
-    # for j in range(12): 
-    #     print(Jan.iloc[yearIndex][0])
-    #     # append to year array
-    #     year2018.append(Jan.iloc[i][0])
-    #     year2018.append(Dec.iloc[i][0])
-    #     # continue for rest of 12 months
-    #     year2008.append(Jan.iloc[i][0])
-    #     year2008.append(Dec.iloc[i][0])
-print(allYears)
-year2018 = allYears[0:12]
-year2008 = allYears[12:24]
-year1998 = allYears[24:36]
-year1988 = allYears[36:48]
-print(year2018)
-print(year2008)
-print(year1998)
-print(year1988)
-# yosemite_data['jan'] = yosemite_data['JAN'].str.split(',')
-# print('here ye :\n',yosemite_data.loc[2018][:])
-N = len(yosemite_data)
-# for i in range(N):
-#     print(Jan.iloc[i][0])
-# print('here ye :\n',Jan.iloc[0][0])
-
-# year2018 = yosemite_data.loc[2018]
-# # print(yosemite_data.JAN.head(3))
-# print(year2018)
-# print(yosemite_data)
-# deg = 1
-# A = A_mat(x_vals, deg)
-
-
-
-# x_vals_2018 = [1,2,3,4,5,6,7,8,9,10,11,12]
-# y_vals_2018 = yosemite_data[0]
-# plt.scatter( x_vals, y_vals, color = 'b', marker = (5, 1))
-# plt.plot(x_vals_2018, y_vals_2018, color = "b") 
-# plt.plot(x_vals, y_pred, color = "r") 
-# plt.plot(x_vals, y_pred, color = "g") 
-# plt.plot(x_vals, y_pred, color = "p") 
-# plt.title('Yosemite Visitors in Various Years')
-# plt.show()
+#     used_years.append(year)
+#     deg_list = []
+#     rmse_list = []
+#     func_list= []
+#     testingYear = yosemite_data.loc[year,:]
+#     testingYear = np.c_[testingYear]
+    
+#     for deg in range(1,21):
+#         x = np.linspace(1,20, 1)
+#         rmse = RMSE(x_vals,testingYear,weights)
+#         func = LLS_func(x_vals, testingYear, weights, deg)
+#         deg_list.append(deg)
+#         rmse_list.append(rmse)
+#         func_list.append(func)
+#     plt.scatter(deg_list, func_list, color = 'blue', marker = '*', s = 40) 
+#     plt.plot(deg_list, func_list, color = "blue", label = 'Training Error')
+#     plt.scatter(deg_list, rmse_list, color = 'orange', marker = 'o',s = 40) 
+#     plt.plot(deg_list, rmse_list, color = "orange", label = 'RMSE')      
+#     handles, labels = plt.gca().get_legend_handles_labels()
+#     plt.legend(handles, labels, loc = 'upper left')
+#     plt.title('Training vs. RMSE for Yosemite Visitors %d' % year)
+#     # plt.show()
 
 # 2b. Solve the ridge regression regularization fitting for 5 years of data for
 #     a fixed degree n >= 10.  Vary the parameter lam over 20 equally-spaced
 #     values from 0 to 1.  Annotate the plots with this value.  
    
+deg = 10
+lam = np.linspace(0.0,1.0,10)
+print(A[11][0])
+# print(y_vals.shape)
+# ridge = LLS_ridge(x_vals,y_vals,11,.25)
+# print(ridge.shape)
+print(y_vals)
+for i in range(20):
+    plotYears()
+    ridge = LLS_ridge(x_vals,y_vals,deg,lam[i])
+    plt.plot(x_vals, ridge, color = "r", label = 'Ridge, lambda= {:1.2f}'.format(lam[i]))
+    handles, labels = plt.gca().get_legend_handles_labels()
+    plt.legend(handles, labels, loc = 'upper left')
+    plt.title('Yosemite Visitors Ridge Regression (%d Degree)' % deg)
+    # plt.show()
+
+    
+
